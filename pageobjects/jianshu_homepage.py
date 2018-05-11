b652699595ea201b0e0684a8d8a036f88c096409
guanzhongpeng2 <guanzhongpeng2@qq.com>
2018-05-12 02:37:28 +0800

    remove classmethod and update login test case

--- pageobjects/jianshu_homepage.py
+++ pageobjects/jianshu_homepage.py
@@ -5,11 +5,13 @@ from framework.base_page import BasePage
 
 class HomePage(BasePage):
 
+    # 定位器
     input_box = "id=>q"
     search_submit_btn = "class_name=>search-btn"
     # 登录入口
     new_link = "xpath=>/html/body/nav/div/a[4]"
 
+    # Action
     def type_search(self, text):
         self.type(self.input_box, text)
 
