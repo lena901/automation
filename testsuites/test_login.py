e3cb467cccb79c2dd645d6a4b19f9667e7161a5a
guanzhongpeng2 <guanzhongpeng2@qq.com>
2018-05-12 01:44:16 +0800

    remove classmethod and update login test case

--- testsuites/test_login.py
+++ testsuites/test_login.py
@@ -3,38 +3,38 @@
 import time
 import unittest
 from framework.browser_engineer import BrowserEngine
-from pageobjects.jianshu_loginpge import LoginPage
 from pageobjects.jianshu_homepage import HomePage
 from framework.data_engineer import ExcelUtil
 import os
 import ddt
-from framework.loginpublic import Login
+from framework.login_go import LoginGo
 
 # 测试数据
 filePath = os.path.dirname(os.path.abspath('.')) + "/config/test_data.xlsx"
 sheetName = "login"
 data = ExcelUtil(filePath, sheetName)
 testData = data.dict_data()
+print(testData)
 
 @ddt.ddt
 class JianShuLogin(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
+    # @classmethod
+    def setUp(self):
         """
         测试固件的setUp()的代码，主要是测试的前提准备工作
         :return:
         """
-        browse = BrowserEngine(cls)
-        cls.driver = browse.open_browser(cls)
+        browser = BrowserEngine(self)
+        self.driver = browser.open_browser(self)
         # 初始化简书首页，并点击登录
-        homepage = HomePage(cls.driver)
+        homepage = HomePage(self.driver)
         # homepage.click_login()
-        cls.driver.find_element_by_xpath('/html/body/nav/div/a[4]').click()
+        self.driver.find_element_by_xpath('/html/body/nav/div/a[4]').click()
         homepage.get_windows_img()
 
-    @classmethod
-    def tearDownClass(cls):
+    # @classmethod
+    def tearDown(self):
         """
         测试结束后的操作，这里基本上都是关闭浏览器
         :return:
@@ -43,15 +43,16 @@ class JianShuLogin(unittest.TestCase):
 
     @ddt.data(*testData)
     def test_login(self, data):
-
+        """登录用例"""
+        print ("当前测试数据%s" % data)
         # 初始化登录首页
-        loginpage = LoginPage(self.driver)
+        logingo = LoginGo(self.driver)
 
-        self.login(data["username"], data["password"])
+        logingo.login(data[u"username"], data[u"password"])
 
-        result = self.is_login_success()
+        result = logingo.is_login_success()
         self.assertTrue(result)
-        loginpage.get_windows_img()
+        logingo.get_windows_img()
 
 
 if __name__ == "__main__":
