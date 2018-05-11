0f065f62eaff7b1f09e17f5588603de4f4c5fd1e
guanzhongpeng2 <guanzhongpeng2@qq.com>
2018-05-11 16:16:21 +0800

    add test_data value and use login method

--- testsuites/test_login.py
+++ testsuites/test_login.py
@@ -2,49 +2,55 @@
 
 import time
 import unittest
-from framework.logger import Logger
 from framework.browser_engineer import BrowserEngine
-from selenium import webdriver
-
 from pageobjects.jianshu_loginpge import LoginPage
-
 from pageobjects.jianshu_homepage import HomePage
+from framework.data_engineer import ExcelUtil
+import os
+import ddt
+from framework.loginpublic import Login
 
+# 测试数据
+filePath = os.path.dirname(os.path.abspath('.')) + "/config/test_data.xlsx"
+sheetName = "login"
+data = ExcelUtil(filePath, sheetName)
+testData = data.dict_data()
+
+@ddt.ddt
 class JianShuLogin(unittest.TestCase):
 
     @classmethod
-    def setUp(cls):
+    def setUpClass(cls):
         """
         测试固件的setUp()的代码，主要是测试的前提准备工作
         :return:
         """
         browse = BrowserEngine(cls)
         cls.driver = browse.open_browser(cls)
+        # 初始化简书首页，并点击登录
+        homepage = HomePage(cls.driver)
+        # homepage.click_login()
+        cls.driver.find_element_by_xpath('/html/body/nav/div/a[4]').click()
+        homepage.get_windows_img()
 
     @classmethod
-    def tearDown(cls):
+    def tearDownClass(cls):
         """
         测试结束后的操作，这里基本上都是关闭浏览器
         :return:
         """
         cls.driver.quit()
 
-    def test_login(self):
-        # 初始化简书首页，并点击登录
-        homepage = HomePage(self.driver)
-        # homepage.click_login()
-        self.driver.find_element_by_xpath('/html/body/nav/div/a[4]').click()
+    @ddt.data(*testData)
+    def test_login(self, data):
 
-        homepage.get_windows_img()
         # 初始化登录首页
         loginpage = LoginPage(self.driver)
-        # self.driver.find_element_by_id("session_email")
-        loginpage.type_username("pp.liu")
-        # self.driver.find_element_by_id("session_password")
-        loginpage.type_password("123456")
-        # self.driver.find_element_by_class_name("sign-in-button")
-        loginpage.send_login_btn()
-        time.sleep(2)
+
+        self.login(data["username"], data["password"])
+
+        result = self.is_login_success()
+        self.assertTrue(result)
         loginpage.get_windows_img()
 
 
